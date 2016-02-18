2a65044c3188ca5a635efa362289523a236864b3
David Victor <david@lionhou.se>
2016-02-18 13:50:31 -0800

    added repeatEndListen

--- src/ng-chocolat.js
+++ src/ng-chocolat.js
@@ -1,54 +1,70 @@
 angular.module('chocolat', [])
-.directive('chocolat', function() {
-    return {
-        // Restrict it to be an attribute in this case
-        restrict: 'A',
+	.directive('chocolat', function () {
+		return {
+			// Restrict it to be an attribute in this case
+			restrict: 'A',
 
-        scope: {
-            /// Instance of the object returned by chocolat to use the api()
-            instance: '=',
-            config: '&',
-            images: '&',
-            container: '@',
-            imageSelector: '@',
-            linkImages: '@',
-            setTitle: '@',
-            className: '@',
-            imageSize: '@',
-            fullScreen: '@',
-            loop: '@',
-            duration: '@',
-            firstImage: '@',
-            lastImage: '@',
-            separator2: '@',
-            setIndex: '@'
-        },
-        // responsible for registering DOM listeners as well as updating the DOM
-        link: function(scope, element, attrs) {
-            var config;
-            if (attrs.config) {
-                config = scope.$eval(scope.config);
-            } else {
-                config =
-                {
-                    images: scope.$eval(scope.images),
-                    container: attrs.container,
-                    imageSelector: attrs.imageSelector,
-                    linkImages: attrs.linkImages,
-                    setTitle: attrs.setTitle,
-                    className: attrs.className,
-                    imageSize: attrs.imageSize,
-                    fullScreen: attrs.fullScreen,
-                    loop: attrs.loop,
-                    duration: attrs.duration,
-                    firstImage: attrs.firstImage,
-                    lastImage: attrs.lastImage,
-                    separator2: attrs.separator2,
-                    setIndex: attrs.setIndex
-                };
-            }
+			scope: {
+				repeatEndListen: '&',
+				/// Instance of the object returned by chocolat to use the api()
+				instance: '=',
+				config: '&',
+				images: '&',
+				container: '@',
+				imageSelector: '@',
+				linkImages: '@',
+				setTitle: '@',
+				className: '@',
+				imageSize: '@',
+				fullScreen: '@',
+				loop: '@',
+				duration: '@',
+				firstImage: '@',
+				lastImage: '@',
+				separator2: '@',
+				setIndex: '@'
+			},
 
-            scope.instance = $(element).Chocolat(config).data('chocolat');
-        }
-    };
-});
+			// responsible for registering DOM listeners as well as updating the DOM
+			link: function (scope, element, attrs) {
+				var config;
+				if (attrs.config) {
+					config = scope.$eval(scope.config);
+				}
+				else {
+					config =
+					{
+						images: scope.$eval(scope.images),
+						container: attrs.container,
+						imageSelector: attrs.imageSelector,
+						linkImages: attrs.linkImages,
+						setTitle: attrs.setTitle,
+						className: attrs.className,
+						imageSize: attrs.imageSize,
+						fullScreen: attrs.fullScreen,
+						loop: attrs.loop,
+						duration: attrs.duration,
+						firstImage: attrs.firstImage,
+						lastImage: attrs.lastImage,
+						separator2: attrs.separator2,
+						setIndex: attrs.setIndex
+					};
+				}
+				//
+				//attrs.$observe('images', function() {
+				//   scope.image = scope.$eval(scope.images);
+				//});
+				//$timeout(function () {
+				//   scope.instance = $(element).Chocolat(config).data('chocolat');
+				//});
+				if (attrs.repeatEndListen) {
+					scope.$on('RepeatEnd', function (event) {
+						scope.instance = $(element).Chocolat(config).data('chocolat');
+					});
+				}
+				else {
+					scope.instance = $(element).Chocolat(config).data('chocolat');
+				}
+			}
+		};
+	});
